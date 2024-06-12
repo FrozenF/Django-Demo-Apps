2408d0618af264a42b083af9a7d84bc43f62b795
farih <farih@btech.id>
2024-06-12 06:25:07 +0000

    Add Unit Case

--- portofolio/tests.py
+++ portofolio/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import PortofolioItem
 
-# Create your tests here.
+class PortofolioItemModelTest(TestCase):
+
+    def setUp(self):
+        self.item = PortofolioItem.objects.create(
+            title='Test Title',
+            description='Test Description',
+            image='path/to/image.jpg'
+        )
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.item), self.item.title)
+
+    def test_title_max_length(self):
+        max_length = self.item._meta.get_field('title').max_length
+        self.assertEqual(max_length, 200)
